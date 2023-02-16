e8f170ecbb2def6d964ff5a80a787b90576007e4
Marco Crisostomi <m.crisostomi91@gmail.com>
2023-02-16 22:07:56 +0100

    add function to change the element background and measure the viewing time

--- src/index.js
+++ src/index.js
@@ -10,11 +10,18 @@ const configurations = {
 
 let myDivC_1 = document.createElement('div');
 myDivC_1.id = "C_1";
+myDivC_1.setAttribute('counter', 0);
 myDivC_1.innerHTML = 'I am the first div called C_1';
 
 let myDivC_2 = document.createElement('div');
 myDivC_2.id = "C_2";
+myDivC_2.setAttribute('counter', 0);
 myDivC_2.innerHTML = 'I am the second div called C_2';
+
+let viewsCounterC_1 = myDivC_1.getAttribute('counter');
+let viewsCounterC_2 = myDivC_2.getAttribute('counter');
+let indexColorC_1 = 0;
+let indexColorC_2 = 0;
 
 // function for replace the selected elements as soon as the DOM is ready
 document.addEventListener('DOMContentLoaded', () => {
@@ -24,7 +31,7 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 // Create the observer and pass it a callback function to be run whenever the threshold that is set the options is crossed
-function createObserver() {
+const createObserver = () => {
     let observer;
     let options = {
         root: null,
@@ -37,13 +44,40 @@ function createObserver() {
     observer.observe(myDivC_2);
 };
 
-// function function to know if an element is visible
+// function to know if an element is visible, change the background color and measure the viewing time of the element
 function handleIntersect(entries) {
     entries.forEach((entry) => {
-        if(entry.intersectionRatio >= 0.8) {
-            console.log("I am visible");
+        let element = entry.target;
+
+        if(entry.isIntersecting) {
+            if(entry.intersectionRatio >= 0.8 && element.id == 'C_1') {
+                element.setAttribute('startViewTimestamp', Date.now().toString());
+                viewsCounterC_1++;
+                if(viewsCounterC_1 == configurations.CHANGE_COLOR_AFTER_VIEWS) {
+                    indexColorC_1++;
+                    viewsCounterC_1 = 0;
+                }
+            } else {
+                element.setAttribute('startViewTimestamp', Date.now().toString());
+                viewsCounterC_2++;
+                if(viewsCounterC_2 == configurations.CHANGE_COLOR_AFTER_VIEWS) {
+                    indexColorC_2++;
+                    viewsCounterC_2 = 0;
+                }
+            }
         } else {
-            console.log("I am hide");
+            const startViewTimestamp = parseInt(element.getAttribute('startViewTimestamp'));
+            console.log(`Element ${element.id} has been viewed for ${(Date.now() - startViewTimestamp) / 1000}s`)
         }
+
+        if(indexColorC_1 > configurations.COLORS.length -1) {
+            indexColorC_1 = 0;
+        }
+        myDivC_1.style.backgroundColor = configurations.COLORS[indexColorC_1];
+
+        if(indexColorC_2 > configurations.COLORS.length -1) {
+            indexColorC_2 = 0;
+        }
+        myDivC_2.style.backgroundColor = configurations.COLORS[indexColorC_2];
     })
 };
