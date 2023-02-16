d0a561f8109afdacc828533e8b2d1e1daaf4afcd
Marco Crisostomi <m.crisostomi91@gmail.com>
2023-02-16 19:50:57 +0100

    add function to know if an element is visible

--- src/index.js
+++ src/index.js
@@ -20,5 +20,30 @@ myDivC_2.innerHTML = 'I am the second div called C_2';
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.replace-c_1').replaceWith(myDivC_1);
     document.querySelector('.replace-c_2').replaceWith(myDivC_2);
+    createObserver();
 });
 
+// Create the observer and pass it a callback function to be run whenever the threshold that is set the options is crossed
+function createObserver() {
+    let observer;
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.8
+    };
+    observer = new IntersectionObserver(handleIntersect, options);
+
+    observer.observe(myDivC_1);
+    observer.observe(myDivC_2);
+};
+
+// function function to know if an element is visible
+function handleIntersect(entries) {
+    entries.forEach((entry) => {
+        if(entry.intersectionRatio >= 0.8) {
+            console.log("I am visible");
+        } else {
+            console.log("I am hide");
+        }
+    })
+};
