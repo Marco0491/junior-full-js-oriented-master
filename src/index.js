d6233eac6866de1d27f4cc2b692f9131704ace95
Marco Crisostomi <m.crisostomi91@gmail.com>
2023-02-16 19:42:27 +0100

    add function for replace element when the DOM is ready

--- src/index.js
+++ src/index.js
@@ -8,5 +8,17 @@ const configurations = {
     ]
 };
 
+let myDivC_1 = document.createElement('div');
+myDivC_1.id = "C_1";
+myDivC_1.innerHTML = 'I am the first div called C_1';
 
-// Add missing code ...
+let myDivC_2 = document.createElement('div');
+myDivC_2.id = "C_2";
+myDivC_2.innerHTML = 'I am the second div called C_2';
+
+// function for replace the selected elements as soon as the DOM is ready
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.replace-c_1').replaceWith(myDivC_1);
+    document.querySelector('.replace-c_2').replaceWith(myDivC_2);
+});
+
